2ba5d08dabc1c549c83094e6aa0e02bf52f2df38
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-02-23 17:00:28 +0200

    Added models to admin page

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from theatre.models import (
+    TheatreHall,
+    Performance,
+    Reservation,
+    Ticket,
+    Actor,
+    Genre,
+    Play,
+)
 
-# Register your models here.
+
+@admin.register(TheatreHall)
+class TheatreHallAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    pass
--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -141,7 +141,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ("theatre.permissions.IsAdminOrIfAuthenticatedReadOnly",),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "theatre.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
